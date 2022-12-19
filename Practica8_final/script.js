b25d7781c5d11e1c95ef3abc3375e7e901275f19
ayoubha <ayoub.hammani@e-campus.uab.cat>
2022-12-19 22:32:45 +0100

    Versió Final Conjunta

---
+++ Practica8_final/script.js
@@ -0,0 +1,10 @@
+function desplegable() {
+    var x = document.getElementById("desple");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
